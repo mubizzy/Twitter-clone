c8543de3be9b0e672603a2f761e9dd01f363e6b4
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-01-11 13:06:39 +0100

    working on the sidebarMenu

--- components/SidebarMenuItem.jsx
+++ components/SidebarMenuItem.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const SidebarMenuItem = () => {
+const SidebarMenuItem = ({ text, Icon, active }) => {
   return <div>SidebarMenuItem</div>;
 };
+
+export default function SidebarMenuItem() {
+  return (
+    <div className="hoverEffect flex items-center text-gray-700 justify-center xl:justify-start text-lg space-x-3">
+      <Icon className="h-7" />
+      <span className={`${active && "font-bold"} hidden xl:inline`}>{text}</span>
+    </div>
 
 export default SidebarMenuItem;
