ef35e238ca3ff6b8bfc8f07e319011fba3960d57
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-01-12 18:18:31 +0100

    working with the title feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { SparklesIcon } from "@heroicons/react/outline";
 
 const Feed = () => {
   return (
-    <div className="xl:ml-[370px] border-l border-r border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl"></div>
+    <div className="xl:ml-[370px] border-l border-r border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
+      <div className="hoverEffect flex items-center justify-center px-0 ml-auto w-9 h-9">
+        <SparklesIcon className="h-5" />
+      </div>
+    </div>
   );
 };
 
