99152007d36ca1c1e6282752395f1e1c6faaf495
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-01-12 17:59:11 +0100

    creation of feed file

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Feed from "../components/Feed";
 import Sidebar from "../components/Sidebar";
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
       <Sidebar />
 
       {/* Feed */}
+      <Feed />
 
       {/* Widgets */}
     </div>
