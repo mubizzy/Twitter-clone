681cdad62d75d4d5331f272bd383005ae79a678e
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-01-14 12:15:18 +0100

    wrking on the text area

--- components/Input.jsx
+++ components/Input.jsx
@@ -3,7 +3,18 @@ import React from "react";
 const Input = () => {
   return (
     <div>
-      <img src="/public/pic.jpeg" alt="" />
+      <img src="/public/pic.jpeg" alt="pics" />
+      <div className="w-full divide-y divide-gray-200">
+        <div className="">
+          <textarea
+            className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700"
+            rows="2"
+            placeholder="What's happening?"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          ></textarea>
+        </div>
+      </div>
     </div>
   );
 };
