262d61b0db6f93062a37357947bcd96db66c688d
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-01-17 11:36:27 +0100

    fetching a dummy post

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,15 +3,29 @@ import { SparklesIcon } from "@heroicons/react/outline";
 import Input from "./Input";
 
 const Feed = () => {
-  const post =[
+  const post = [
     {
-      id:"`1",
-      name:"Mubarak Ajibola",
-      username:"mubizy",
-      userimg:"",
-
-    }
-  ]  return (
+      id: "1",
+      name: "Mubarak Ajibola",
+      username: "mubizzy",
+      userimg:
+        "https://images.unsplash.com/photo-1531891437562-4301cf35b7e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Z3V5fGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png",
+      text: "nice view!",
+      timestamp: "2 hours ago",
+    },
+    {
+      id: "1",
+      name: "Mubarak Ajibola",
+      username: "mubizzy",
+      userimg:
+        "https://images.unsplash.com/photo-1531891437562-4301cf35b7e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Z3V5fGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png",
+      text: "nice view!",
+      timestamp: "2 hours ago",
+    },
+  ];
+  return (
     <div className="xl:ml-[370px] border-l border-r border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
       <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
         <h2 className="text-lg sm:text-xl font-bold cursor-pointer">Home</h2>
