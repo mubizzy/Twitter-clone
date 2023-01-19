5cc25a6b22f8773b60aed60bc96b8c8131abcda6
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-01-19 14:35:43 +0100

    styling a widget

--- components/Widgets.jsx
+++ components/Widgets.jsx
@@ -1,13 +1,16 @@
 import { SearchIcon } from "@heroicons/react/outline";
-import React from "react";
 
 const Widgets = () => {
   return (
-    <div>
+    <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
       <div className="">
         <div className="">
-          <SearchIcon className="h-5" />
-          <input type="text" placeholder="Search Twitter" />
+          <SearchIcon className="h-5  text-gray-500" />
+          <input
+            type="text"
+            placeholder="Search Twitter"
+            className="absolute inset-0 rounded-full pl-11 border-gray-500 text-gray-700 focus:shadow-lg focus:bg-white bg-gray-100"
+          />
         </div>
       </div>
     </div>
