dd2ec208fa3edb64c540224bfca25ec747c11bd9
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-01-20 11:48:32 +0100

    working on the news content

--- components/News.jsx
+++ components/News.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const News = ({ article }) => {
-  return <div></div>;
+  return (
+    <a rel="noreferrer" href={article.url} target="_blank">
+      {" "}
+      <div className="space-y-0.5">
+        <h6 className="text-sm font-bold">{article.title}</h6>
+        <p className="text-xs font-medium text-gray-500">
+          {article.source.name}
+        </p>
+      </div>
+    </a>
+  );
 };
 
 export default News;
