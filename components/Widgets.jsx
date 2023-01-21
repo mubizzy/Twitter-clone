f91c9a518610a12c174ffa945dd7b14983b349f2
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-01-21 12:48:05 +0100

    random users api styling(widgets)

--- components/Widgets.jsx
+++ components/Widgets.jsx
@@ -46,12 +46,16 @@ const Widgets = ({ newsResults, randomUsersResults }) => {
               width="40"
             />
             <div className="">
-              <h4 className="">{randomUser.login.username}</h4>
-              <h5 className="">
+              <h4 className="font-bold hover:underline text-[14px] truncate">
+                {randomUser.login.username}
+              </h4>
+              <h5 className="text-[13px] text-gray-500 truncate">
                 {randomUser.name.first + " " + randomUser.name.last}
               </h5>
             </div>
-            <button className="">follow</button>
+            <button className="ml-auto bg-black text-white rounded-full text-sm px-3.5 py-1.5 font-bold">
+              follow
+            </button>
           </div>
         ))}
         <div className="flex items-center px-4 py-2  cursor-pointer hover:bg-gray-200 transition duration-500 ease-out"></div>
