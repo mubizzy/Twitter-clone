2c57658ed541846f000ca23faae6660f263f7545
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-01-21 12:45:50 +0100

    random users api (widgets)

--- components/Widgets.jsx
+++ components/Widgets.jsx
@@ -35,8 +35,23 @@ const Widgets = ({ newsResults, randomUsersResults }) => {
           Show more
         </button>
         {randomUsersResults.map((randomUser) => (
-          <div key={randomUser.login.username}>
-            <img src={randomUser.picture.thumbnail} alt="" />
+          <div
+            key={randomUser.login.username}
+            className="flex items-center px-4 py-2  cursor-pointer hover:bg-gray-200 transition duration-500 ease-out"
+          >
+            <img
+              src={randomUser.picture.thumbnail}
+              alt=""
+              className="rounded-full"
+              width="40"
+            />
+            <div className="">
+              <h4 className="">{randomUser.login.username}</h4>
+              <h5 className="">
+                {randomUser.name.first + " " + randomUser.name.last}
+              </h5>
+            </div>
+            <button className="">follow</button>
           </div>
         ))}
         <div className="flex items-center px-4 py-2  cursor-pointer hover:bg-gray-200 transition duration-500 ease-out"></div>
