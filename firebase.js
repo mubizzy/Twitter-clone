e38da4cb87f499f93c983da40f287e6837262cb3
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-01-22 12:52:10 +0100

    create a firebase app

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDeumlvHaFHCCMQrKGhgueuoIgGfhj7pD0",
+  authDomain: "twitter-v1-b2008.firebaseapp.com",
+  projectId: "twitter-v1-b2008",
+  storageBucket: "twitter-v1-b2008.appspot.com",
+  messagingSenderId: "971482729443",
+  appId: "1:971482729443:web:3668cfbe2adcf22c4faed6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
