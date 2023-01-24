d6f9241619b510e4dd78b43b98fa183bc4faa697
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-01-24 12:26:41 +0100

    working on the signin file3

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,13 +1,10 @@
-import { getProviders } from "next-auth/react";
-
-const signin = ({ providers }) => {
+const signin = () => {
   return (
-    <div>
-      {" "}
+    <div className="flex justify-center mt-20 space-x-4">
       <img
         src="https://cdn.cms-twdigitalassets.com/content/dam/help-twitter/en/twitter-tips/desktop-assets/ch-01/ch12findphone.png.twimg.1920.png"
         alt="twitter image inside a phone"
-        className=""
+        className="hidden object-cover md:w-44 md:h-80 rotate-6  md:inline-flex"
       />
       <div className="">
         <div className="flex flex-col items-center">
@@ -19,10 +16,7 @@ const signin = ({ providers }) => {
           <p className="text-center text-sm italic my-10">
             This app is created for learning purposes
           </p>
-          <button
-            onClick={onGoogleClick}
-            className="bg-red-400 rounded-lg p-3 text-white hover:bg-red-500"
-          >
+          <button className="bg-red-400 rounded-lg p-3 text-white hover:bg-red-500">
             Sign in with Google
           </button>
         </div>
@@ -32,12 +26,3 @@ const signin = ({ providers }) => {
 };
 
 export default signin;
-
-export async function getServerSideProps() {
-  const providers = await getProviders();
-  return {
-    props: {
-      providers,
-    },
-  };
-}
