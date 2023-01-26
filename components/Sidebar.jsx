827b822cf5ef5287f5cf66ef08b59d3ad9a18309
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-01-26 17:29:52 +0100

    working on the sidebar functionality 2

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -44,22 +44,29 @@ const Sidebar = () => {
           )}
         </div>
         {/* Button */}
-        <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
-          Tweet
-        </button>
-        {/* Mini-Profile */}
-        <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
-          <img
-            src="https://images.unsplash.com/photo-1531891437562-4301cf35b7e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Z3V5fGVufDB8fDB8fA%3D%3D&w=1000&q=80"
-            alt="user-img"
-            className="h-10 w-10 rounded-full xl:mr-2"
-          />
-          <div className="leading-5 hidden xl:inline">
-            <h4 className="font-bold"></h4>
-            <p className="text-gray-500"></p>
-          </div>
-          <DotsHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" />
-        </div>
+        {session ? (
+          <>
+            {" "}
+            <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
+              Tweet
+            </button>
+            {/* Mini-Profile */}
+            <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
+              <img
+                src="https://images.unsplash.com/photo-1531891437562-4301cf35b7e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Z3V5fGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+                alt="user-img"
+                className="h-10 w-10 rounded-full xl:mr-2"
+              />
+              <div className="leading-5 hidden xl:inline">
+                <h4 className="font-bold"></h4>
+                <p className="text-gray-500"></p>
+              </div>
+              <DotsHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" />
+            </div>
+          </>
+        ) : (
+          <button>sigin in</button>
+        )}
       </div>
     </main>
   );
