0708214971c2e3f5ab879da9acc00abfdb0813f0
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-01-26 18:49:18 +0100

    working on the input functionality

--- components/Input.jsx
+++ components/Input.jsx
@@ -1,9 +1,12 @@
 import { EmojiHappyIcon, PhotographIcon } from "@heroicons/react/outline";
 import { useSession, signOut } from "next-auth/react";
+import { useState } from "react";
 
 const Input = () => {
   const { data: session } = useSession();
   console.log(session);
+  const [input, setInput] = useState("");
+
   return (
     <>
       {" "}
@@ -21,6 +24,8 @@ const Input = () => {
                 className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700"
                 rows="2"
                 placeholder="What's happening?"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
               ></textarea>
             </div>
             <div className="flex items-center justify-between pt-2.5">
@@ -28,7 +33,11 @@ const Input = () => {
                 <PhotographIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
                 <EmojiHappyIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
               </div>
-              <button className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50">
+              <button
+                onClick={sendPost}
+                disabled={!input.trim()}
+                className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50"
+              >
                 Tweet
               </button>
             </div>
