1b04b2fc91d5c2e09f01b8ee7ad2af042db99dfd
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-01-26 18:49:34 +0100

    working on the sidebar functionality 5

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -25,6 +25,7 @@ const Sidebar = () => {
             width="50"
             height="50"
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/800px-Twitter-logo.svg.png"
+            alt=""
           ></Image>
         </div>
         {/* Menu */}
