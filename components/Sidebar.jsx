7f72d03d40900fa78102717c5ac7ab7870344ed8
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-01-26 17:22:42 +0100

    working on the sidebar functionality

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -31,12 +31,17 @@ const Sidebar = () => {
         <div className="mt-4 mb-2.5 xl:items-start">
           <SidebarMenuItem text="Home" Icon={HomeIcon} />
           <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
-          <SidebarMenuItem text="Notifications" Icon={BellIcon} />
-          <SidebarMenuItem text="Messages" Icon={InboxIcon} />
-          <SidebarMenuItem text="Bookmarks" Icon={BookmarkIcon} />
-          <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
-          <SidebarMenuItem text="Profile" Icon={UserIcon} />
-          <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon} />
+          {session && (
+            <>
+              {" "}
+              <SidebarMenuItem text="Notifications" Icon={BellIcon} />
+              <SidebarMenuItem text="Messages" Icon={InboxIcon} />
+              <SidebarMenuItem text="Bookmarks" Icon={BookmarkIcon} />
+              <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
+              <SidebarMenuItem text="Profile" Icon={UserIcon} />
+              <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon} />
+            </>
+          )}
         </div>
         {/* Button */}
         <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
