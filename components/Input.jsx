c32b935b675df2d2ce682db019326ef10c31bc75
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-01-31 13:21:26 +0100

    get post data from firestore and show them in the post section

--- components/Input.jsx
+++ components/Input.jsx
@@ -56,9 +56,8 @@ const Input = () => {
       });
     }
     setInput("");
-
     setSelectedFile(null);
-    // setLoading(false);
+    setLoading(false);
   };
 
   return (
@@ -95,28 +94,36 @@ const Input = () => {
                 />
               </div>
             )}
-            <div className="flex items-center justify-between pt-2.5">
-              <div className=" flex">
-                <div className="" onClick={() => filePickerRef.current.click()}>
-                  {" "}
-                  <PhotographIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
-                  <input
-                    type="file"
-                    hidden
-                    ref={filePickerRef}
-                    onChange={addImageToPost}
-                  />
-                </div>
 
-                <EmojiHappyIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
-              </div>
-              <button
-                onClick={sendPost}
-                disabled={!input.trim()}
-                className="bg-blue-4 00 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50"
-              >
-                Tweet
-              </button>
+            <div className="flex items-center justify-between pt-2.5">
+              {!loading && (
+                <>
+                  <div className=" flex">
+                    <div
+                      className=""
+                      onClick={() => filePickerRef.current.click()}
+                    >
+                      {" "}
+                      <PhotographIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
+                      <input
+                        type="file"
+                        hidden
+                        ref={filePickerRef}
+                        onChange={addImageToPost}
+                      />
+                    </div>
+
+                    <EmojiHappyIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
+                  </div>
+                  <button
+                    onClick={sendPost}
+                    disabled={!input.trim()}
+                    className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50"
+                  >
+                    Tweet
+                  </button>
+                </>
+              )}
             </div>
           </div>
         </div>
