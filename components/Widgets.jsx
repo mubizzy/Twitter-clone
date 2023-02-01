34c83b84ffff68c9445d53ef1a2bf00cc9e54a7a
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-02-01 12:20:11 +0100

    adding framer motion 6

--- components/Widgets.jsx
+++ components/Widgets.jsx
@@ -1,4 +1,5 @@
 import { SearchIcon } from "@heroicons/react/outline";
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 import News from "./News";
 
@@ -19,9 +20,19 @@ const Widgets = ({ newsResults, randomUsersResults }) => {
       </div>
       <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]">
         <h4 className="font-bold text-xl px-4">Whats happening</h4>
-        {newsResults.slice(0, articleNum).map((article) => (
-          <News key={article.title} article={article} />
-        ))}
+        <AnimatePresence>
+          {newsResults.slice(0, articleNum).map((article) => (
+            <motion.div
+              key={article.title}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 1 }}
+            >
+              <News key={article.title} article={article} />
+            </motion.div>
+          ))}
+        </AnimatePresence>
 
         <button
           onClick={() => setArticleNum(articleNum + 3)}
