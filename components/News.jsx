57d7035b3f313d20b0209e2006027ba47cbccbf6
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-02-01 12:13:34 +0100

    adding framer motion 5

--- components/News.jsx
+++ components/News.jsx
@@ -2,10 +2,8 @@ import React from "react";
 
 const News = ({ article }) => {
   return (
-    <a rel="noreferrer" href={article.url} target="_blank">
-      {" "}
+    <a href={article.url} target="_blank">
       <div className="flex items-center justify-between px-4 py-2 space-x-1 hover:bg-gray-200 transition duration-500 ease-out">
-        {" "}
         <div className="space-y-0.5">
           <h6 className="text-sm font-bold">{article.title}</h6>
           <p className="text-xs font-medium text-gray-500">
