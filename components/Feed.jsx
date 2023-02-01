48fa8614e5f6be9db674554472a53950223444b5
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-02-01 12:03:53 +0100

    adding framer motion

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,23 +1,31 @@
 import React from "react";
 import { SparklesIcon } from "@heroicons/react/outline";
-import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
-import { db } from "../firebase";
-import { useEffect, useState } from "react";
 import Input from "./Input";
 import Post from "./Post";
 
 const Feed = () => {
-  const [posts, setPosts] = useState([]);
-  useEffect(
-    () =>
-      onSnapshot(
-        query(collection(db, "posts"), orderBy("timestamp", "desc")),
-        (snapshot) => {
-          setPosts(snapshot.docs);
-        }
-      ),
-    []
-  );
+  const posts = [
+    {
+      id: "1",
+      name: "Mubarak Ajibola",
+      username: "mubizzy",
+      userimg:
+        "https://images.unsplash.com/photo-1531891437562-4301cf35b7e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Z3V5fGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png",
+      text: "nice view!",
+      timestamp: "2 hours ago",
+    },
+    {
+      id: "2",
+      name: "Mubarak Ajibola",
+      username: "mubizzy",
+      userimg:
+        "https://images.unsplash.com/photo-1531891437562-4301cf35b7e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Z3V5fGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+      img: "https://media.istockphoto.com/id/1094390150/photo/suspension-bridge-with-lights-sunset-on-a-bridge-lagos-nigeria.jpg?s=612x612&w=0&k=20&c=Y_MSbC4yLGGU2i6x48eKL4qzMTWsgg4KRHT4rmU1mYs=",
+      text: "wow!",
+      timestamp: "1 hours ago",
+    },
+  ];
   return (
     <div className="xl:ml-[370px] border-l border-r border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
       <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
@@ -27,9 +35,19 @@ const Feed = () => {
         </div>
       </div>
       <Input />
-      {posts.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      <AnimatePresence>
+        {posts.map((post) => (
+          <motion.div
+            key={post.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+          >
+            <Post key={post.id} post={post} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
 };
