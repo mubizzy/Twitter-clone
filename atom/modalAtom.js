558b21c0befb773532aece535b89df5db2e35801
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-02-02 19:57:49 +0100

    comment section 2

--- atom/modalAtom.js
+++ atom/modalAtom.js
@@ -1,5 +1,5 @@
 import { atom } from "recoil";
 export const modalState = atom({
   key: "modalState", // unique ID (with respect to other atoms/selectors)
-  default: false, // default value (aka initial value)
+  default: "id", // default value (aka initial value)
 });
