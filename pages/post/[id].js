680604e18b8feb48b0bb67d63bde5f1cfead40eb
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-02-05 15:32:17 +0100

    js changes

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,5 +1,12 @@
 import React from "react";
 import { useRouter } from "next/router";
+import {
+  collection,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 
 export const [id] = () => {
   return <div>[id]</div>;
